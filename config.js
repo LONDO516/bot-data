93b51682c84c01411f4c5bc6d4ab2724ce2a4606
LONDO516 <diangsvshs@gmail.com>
2026-02-01 09:33:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+/* <--! CREATED BY LONDO !-->
+
+Telegram Developer @londohost
+Join Channel Info Update @Londohost
+
+*/
+module.exports = {
+  BOT_TOKEN: "8231824587:AAGq-VaL6kCOcnxAB2735FuyeBQmH5qGYkY",
+  OWNER_ID: ["8485662776"],
+};
